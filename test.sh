b1cf2ffbd1488ecb1bba6ef4e2573f6c433c2b30
Casey Getz <cgetz@linkedin.com>
2022-04-05 15:41:44 -0700

    test push

--- test.sh
+++ test.sh
@@ -5,3 +5,9 @@ echo $ENV2
 
 echo "Showing tags:"
 git tag
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+git tag -a -m "Release v123" "v123"
+git push --tags
+
